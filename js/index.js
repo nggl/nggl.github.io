fdf9db7d1ec5e7e595296c3ddad3a27e996ee020
nggl mac <ngfrankgl@163.com>
2020-08-21 14:39:31 +0800

    update expiress

--- js/index.js
+++ js/index.js
@@ -31,8 +31,13 @@ new Vue({
 			tip: '使用uni-app框架和vue-cli进行项目开发；使用Element-ui进行后台管理系统的搭建；使用Less规范进行样式编写；JavaScript代码规范采用ES5和ES6混合的面向过程风格；组件化页面编写、前端路由。',
 		}],
 		projects: [{
+				name: '秀吧带货',
+				time: '2019.07-2019.12',
+				worker: 'web前端',
+				tip: '原生微信小程序，标准化的编写，使用了es6语法和weapp小程序ui组件',
+			},{
 			name: '信书分享h5和小程序',
-			time: '2019.05-2019.09',
+			time: '2019.05-2019.07',
 			woker: 'web前端',
 			tip: '使用uni-app框架进行项目的搭建；使用Less规范进行样式编写；使用uni-app提供的全平台兼容的UI框架。',
 			/*urls: [{
@@ -73,14 +78,14 @@ new Vue({
 			edu: '本科',
 		},
 		tip: `有JavaScript语言基础，熟悉ES6以后版本的常用特性；
-				掌握HTML5和CSS3并且满足一般项目需求，包括有，弹性布局、网格布局等；
+				掌握HTML5和CSS3并且满足一般项目需求，包括有，弹性布局、网格布局和多列等；
 				熟练并且能够使用vue前端框架，熟悉React特性，实际上没有工作上的需求；
 				熟悉的后台管理系统UI框架有，element-ui，iview，layui，bootstrap，前端UI框架有，mint-ui，vant；
 				习惯通过less和原生css手写样式效果；
 				使用uniapp和h5+api开发混合app；
-				熟悉nodejs，mangodb，MySQL，PHP并且能够编写一些简单的全栈项目；
-				能够精炼语言，提高代码的可读性和可复用性；
-				能够使用PS进行修图，切图等一般操作和简单的排版；
+				熟悉nodejs，mangodb，MySQL，PHP；
+				能够精炼代码，提高代码的可读性和可复用性；
+				熟练使用PS进行修图、切图等操作；
 				能够协同工作，完成项目需求，代码编写较为严谨，生活乐观`,
 	},
 	methods: {
