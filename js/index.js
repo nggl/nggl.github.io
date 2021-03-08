46ec9603c96b82b4c23bc021fbff8fbf38c6d4fe
nggl mac <ngfrankgl@163.com>
2021-03-08 16:07:50 +0800

    change info and content

--- js/index.js
+++ js/index.js
@@ -15,14 +15,14 @@ new Vue({
 		],
 		expiress: [
 			{
-				name: '湖南云木网络科技有限公司',
+				name: '湖南省云木网络科技有限公司',
 				worker: '前端',
 				// wages: '8k',
 				time: '2020.10-2021.07',
 				tip: '通过uniapp框架，开发ios和android应用',
 			}, 
 			{
-				name: '长沙云中科科技公司',
+				name: '湖南中网科技有限公司',
 				worker: '前端工程师',
 				// wages: '7k',
 				time: '2019.07-2020.09',
@@ -34,10 +34,10 @@ new Vue({
 				time: '2019.04-2019.07',
 				tip: '使用uni-app框架进行项目开发；使用Less规范进行样式编写；JavaScript代码规范采用ES5和ES6混合的面向过程风格；项目使用Vue组件化规范。',
 			}, */{
-				name: '时刻未来教育科技有限公司',
+				name: '湖南时刻未来教育科技有限公司',
 				worker: 'web前端',
 				// wages: '5k',
-				time: '2018.11-2019.04',
+				time: '2018.07-2019.03',
 				// tip: '使用uni-app框架和vue-cli进行项目开发；使用Element-ui进行后台管理系统的搭建；使用Less规范进行样式编写；JavaScript代码规范采用ES5和ES6混合的面向过程风格；组件化页面编写、前端路由。',
 				tip: '使用h5+css的形式进行页面编写，负责web和h5，js库用到了vue.js和jquery',
 			}
@@ -57,7 +57,7 @@ new Vue({
 			},/*{
 				name: '信书分享h5和小程序',
 				time: '2019.05-2019.07',
-				woker: 'web前端',
+				worker: 'web前端',
 				tip: '使用uni-app框架进行项目的搭建；使用Less规范进行样式编写；使用uni-app提供的全平台兼容的UI框架。',
 				// urls: [{
 				// 	name: 'h5在线',
@@ -65,8 +65,8 @@ new Vue({
 				// }],
 			}, */{
 				name: '学成教育',
-				time: '2018.11-2019.04',
-				woker: 'web前端',
+				time: '2018.07-2019.03',
+				worker: 'web前端',
 				// tip: '线上教育平台学程，一套从管理、pc版和移动版的项目；后台使用了Hui框架结合jQuery进行开发；pc站项目使用vue.js、jQuery和layui进行编写；移动版使用vue组件化开发，没有使用vue-cli编写的第一版是陪伴项目，pc和移动版均为前后台分离模式进行开发，项目第二版移动版(客户需要)，采用更为精致的UI图纸进行项目开发(uni-app)。',
 				tip: '线上教育平台，负责前后台项目，使用传统的jQuery形式行进开发，后台管理系统使用asp技术，负责静态页面编写',
 				/*urls: [{
@@ -78,8 +78,8 @@ new Vue({
 				}],*/
 			}, {
 				name: '上海丸旭',
-				time: '2019.04-2019.05',
-				woker: '前端',
+				time: '2018.11-2018.11',
+				worker: 'web前端',
 				tip: 'web项目，企业官网，负责前端的网页（静态页面）编写，后台使用ThinkPHP框架',
 				/*urls: [{
 					name: '官方网站',
@@ -89,7 +89,7 @@ new Vue({
 			/*{
 				name: '基于Android天气预报app',
 				time: '2019.04-2019.05',
-				woker: '全栈',
+				worker: '全栈',
 				tip: '本科生毕业设计项目，使用原生PHP、MySQL技术，vue-cli、uni-app框架，Less规范进行项目开发；项目使用了MVVM的设计模式，灵活运用了PHP语言的变量的灵活性；项目的数据库设计不足10，涉及的业务比较清晰；主要的业务包括，用户登录、注册，用户收藏地址记录，热门城市设置第三方定位API、天气预报API使用。',
 				// urls: [{
 				// 	name: '后台管理',
@@ -100,10 +100,11 @@ new Vue({
 		userinfo: {
 			name: '赵国梁',
 			age: '27',
-			phone: '13135140441',
+			// phone: '13135140441',
+			phone: '18173643868',
 			// mail: '845014705@qq.com',
 			mail: 'ngfrankgl@163.com',
-			exp: '4年',
+			exp: '3年',
 			side: '长沙市岳麓区',
 			worker: '前端工程师',
 			wages: '12k',
